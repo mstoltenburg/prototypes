8a3e774c270d69f8f648b5d5a4186f241bc3ccb1
Moritz Stoltenburg <mstoltenburg@metadesign.de>
2013-12-16 14:27:09 +0100

    initial commit

---
+++ artikel/js/development.js
@@ -0,0 +1,34 @@
+(function() {
+	'use strict';
+
+	var iframe = document.querySelector('iframe'),
+		swap = document.querySelector('.preview-switch');
+
+	// just for development
+	swap.addEventListener('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		var data   = e.target.dataset,
+			width  = data.width,
+			height = data.height,
+			change = data.change;
+
+		if (width) {
+			iframe.width = width;
+			iframe.height = height;
+		} else if (change) {
+			switch (change) {
+				case 'x':
+					top.location.href = iframe.src;
+					break;
+
+				case 'y':
+					iframe.height = '100%';
+					// iframe.style.height = '100%';
+					break;
+			}
+		}
+	});
+
+}());

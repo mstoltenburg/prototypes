a8c2477c287eec539ca3ddcd8e1b42985655ed60
Moritz Stoltenburg <slomo@slomo.de>
2014-02-02 13:05:18 +0100

    initial commit for interview sprint

---
+++ zeit/Gruntfile.js
@@ -0,0 +1,99 @@
+module.exports = function( grunt ) {
+	"use strict";
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON( "package.json" ),
+		jshint: {
+			all: {
+				src: [
+					"src/**/*.js", "test/**/*.js"
+				],
+				options: {
+					"curly": true,
+					"eqeqeq": true,
+					"eqnull": true,
+					"expr": true,
+					"immed": true,
+					"noarg": true,
+					"onevar": true,
+					"quotmark": "double",
+					"smarttabs": true,
+					"trailing": true,
+					"undef": true,
+					"unused": true,
+					"node": true,
+					"browser": true,
+					"devel": true,
+					"globals": {
+						"jQuery": true,
+						"TextSearch": true
+					}
+				}
+			}
+		},
+		uglify: {
+			all: {
+				files: {
+					"build/js/zeit.min.js": [ "src/js/zeit.js" ]
+				},
+				options: {
+					preserveComments: false,
+					report: "min"
+				}
+			}
+		},
+		qunit: {
+			files: [ "test/**/*.html" ]
+		},
+		copy: {
+			css: {
+				expand: true,
+				cwd: "src/",
+				src: "css/*",
+				dest: "build/"
+			},
+			html: {
+				expand: true,
+				cwd: "src/",
+				src: "*.html",
+				dest: "build/"
+			}
+		},
+		sass: {
+			all: {
+				options: {
+					style: "expanded"
+				},
+				files: {
+					"build/css/zeit.css": "src/sass/zeit.scss"
+				}
+			}
+		},
+		watch: {
+			html: {
+				files: [ "src/*.html" ],
+				tasks: "copy:html"
+			},
+			scripts: {
+				files: [ "<%= jshint.all.src %>" ],
+				tasks: "scripts"
+			},
+			styles: {
+				files: [ "src/sass/*.scss" ],
+				tasks: "sass"
+			}
+		}
+	});
+
+	// Load required plugins
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-qunit");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-contrib-sass");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+
+	// Default grunt
+	grunt.registerTask( "scripts", [ "jshint", "uglify"] );
+	grunt.registerTask( "default", [ "jshint", "uglify", "sass", "copy"] );
+};
---
+++ zeit/src/js/zeit.js
@@ -0,0 +1,89 @@
+(function(window) {
+	"use strict";
+
+	var TextSearch = function () {
+		this.init.apply(this, arguments);
+	};
+
+	TextSearch.prototype = {
+		maxInputLength: 200000,
+		input: null,
+		text: null,
+		words: 0,
+		counter: null,
+		query: [],
+		errors: [],
+		result: null,
+
+		init: function(input) {
+			this.input = input.trim();
+			this.parseInput();
+			this.checkInput();
+			this.searchText();
+		},
+
+		parseInput: function() {
+			var re = /([^]+?)\n(\d+)\n([^]+)/,
+				found = this.input.match(re);
+
+			if (found) {
+				this.text = found[1];
+				this.words = this.countWords(this.text);
+				this.counter = parseInt(found[2], 10);
+				this.query = found[3].split("\n");
+			} else {
+				this.errors.push("Die Eingabe entspricht nicht dem Eingabeformat.");
+			}
+		},
+
+		checkInput: function() {
+			if (this.input.length > this.maxInputLength) {
+				this.errors.push("Die Anzahl der eingegebenen Zeichen darf nicht größer als " + this.maxInputLength + " sein.");
+			}
+			if (this.query.length >= this.words) {
+				this.errors.push("Die Anzahl der Suchworte muss kleiner als die Anzahl der Wörter des zu durchsuchenden Text sein.");
+			}
+			if (this.counter !== this.query.length) {
+				this.errors.push("Die angegebene Anzahl der Suchworte (" + this.counter + ") entspricht nicht der Anzahl eingegebener Suchworte (" + this.query.length + ").");
+			}
+		},
+
+		countWords: function(txt) {
+			var words = txt.match(/\S+/g);
+
+			return words ? words.length : 0;
+		},
+
+		searchText: function() {
+			var firstWord = this.query[0],
+				separator = "\\b[^]+?\\b",
+				re = new RegExp("\\b" + this.query.join(separator) + "\\b", "gi"),
+				matches = this.text.match(re);
+
+			if (matches) {
+				console.log(re.toString());
+				console.log(matches);
+				console.log(firstWord);
+
+				this.result = matches[0];
+			} else {
+				this.errors.push("Kein Abschnitt gefunden.");
+			}
+		}
+	};
+
+	window.TextSearch = TextSearch;
+
+})(window);
+
+
+function searchText(e) {
+	e.preventDefault();
+	e.stopPropagation();
+
+	var ts = new TextSearch(this.elements.searchText.value);
+
+	console.log(ts);
+}
+
+document.forms[0].addEventListener("submit", searchText, false);
